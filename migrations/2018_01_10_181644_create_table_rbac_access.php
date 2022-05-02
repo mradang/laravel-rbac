5e4b111fd8b2501ec5eae5d127efab77780aae6b
mradang <mradang@163.com>
2022-05-02 20:24:24 +0800

    优化表迁移、路由和测试

--- migrations/2018_01_10_181644_create_table_rbac_access.php
+++ migrations/2018_01_10_181644_create_table_rbac_access.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableRbacAccess extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateTableRbacAccess extends Migration
     {
         Schema::dropIfExists('rbac_access');
     }
-}
+};
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -5,17 +5,15 @@ use mradang\LaravelRbac\Controllers\RbacNodeController;
 use mradang\LaravelRbac\Controllers\RbacRoleController;
 
 Route::group([
-    'prefix' => 'api',
+    'prefix' => 'api/rbac',
     'middleware' => ['auth:sanctum', 'rbac'],
 ], function () {
-    Route::group(['prefix' => 'rbac'], function () {
-        Route::post('allNodes', [RbacNodeController::class, 'all']);
-        Route::post('allRoles', [RbacRoleController::class, 'all']);
-        Route::post('createRole', [RbacRoleController::class, 'create']);
-        Route::post('deleteRole', [RbacRoleController::class, 'delete']);
-        Route::post('updateRole', [RbacRoleController::class, 'update']);
-        Route::post('findRoleWithNodes', [RbacRoleController::class, 'findWithNodes']);
-        Route::post('saveRoleSort', [RbacRoleController::class, 'saveSort']);
-        Route::post('syncRoleNodes', [RbacRoleController::class, 'syncNodes']);
-    });
+    Route::post('allNodes', [RbacNodeController::class, 'all']);
+    Route::post('allRoles', [RbacRoleController::class, 'all']);
+    Route::post('createRole', [RbacRoleController::class, 'create']);
+    Route::post('deleteRole', [RbacRoleController::class, 'delete']);
+    Route::post('updateRole', [RbacRoleController::class, 'update']);
+    Route::post('findRoleWithNodes', [RbacRoleController::class, 'findWithNodes']);
+    Route::post('saveRoleSort', [RbacRoleController::class, 'saveSort']);
+    Route::post('syncRoleNodes', [RbacRoleController::class, 'syncNodes']);
 });
